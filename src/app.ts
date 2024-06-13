4d3948a39ef85c7514045e14d033ed445eef7dea
ProgrammerShahJalal <you@example.com>
2024-06-13 08:12:33 +0600

    global error handling and not found

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import router from './app/routes';
 import bodyParser from 'body-parser';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
@@ -16,5 +18,11 @@ app.use('/api', router);
 app.get('/', (req: Request, res: Response) => {
   res.send('Meeting Room Booking System for Co-working spaces 🚀');
 });
+
+//global error handler
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
 
 export default app;
