25128894484b0f6df092b7d9e8d0058fa80a5ac5
ProgrammerShahJalal <you@example.com>
2024-08-31 12:37:26 +0600

    allow origin

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ const app: Application = express();
 // CORS configuration
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://meeting-room-booking-frontend.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
